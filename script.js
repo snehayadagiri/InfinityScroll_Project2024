97b7f99fe9bfbdb424dc6d76148881f2421443d9
Sneha Yadagiri <snehayadagiri@Snehas-MacBook-Pro.local>
2025-01-01 14:13:31 -0600

    Wip

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Script.js
+// Unplash API
+const count = 10;
+const apiKey = '';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get Photos for unsplash api.
+async function  getPhotos() {
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log('data', data);
+    } catch (error) {
+        
+    }
+    
+}
+
+// On page load
+getPhotos()
